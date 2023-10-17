89ac53ab574047fbcae53ac1d982b69d68f615a6
Mishab <mishabmsb91@gmail.com>
2023-10-17 23:47:34 +0530

    MORE CHANGES

--- config/collections.js
+++ config/collections.js
@@ -2,6 +2,5 @@ module.exports={
     PRODUCTS_COLLECTION:'products',
     USERS_COLLECTION:'users',
     ADMIN_COLLECTION:'admin',
-    CART_COLLECTION:'cart',
-    ORDER_COLLECTION:'order'
+    CATEGORY_COLLECTION: "category"
 }
